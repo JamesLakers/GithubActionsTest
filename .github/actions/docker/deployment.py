d99c72ef948385016dbf16fb4189a075df82ec44
James <jlogan117@gmail.com>
2024-10-06 14:18:29 -0400

    fixed docker python

--- .github/actions/docker/deployment.py
+++ .github/actions/docker/deployment.py
@@ -9,9 +9,9 @@ def run():
     bucket_region = os.environ['INPUT_BUCKET-REGION']
     dist_folder = os.environ['INPUT_DIST-FOLDER']
 
-    configuration = Config(region_name=bucket_region)
+    # configuration = Config(region_name=bucket_region)
 
-    s3_client = boto3.client('s3', config=configuration)
+    # s3_client = boto3.client('s3', config=configuration)
 
     # for root, subdirs, files in os.walk(dist_folder):
     #     for file in files:
