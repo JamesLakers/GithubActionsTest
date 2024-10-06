b508544074b2e97074cf49ad6769b26d8a0ffca8
James <jlogan117@gmail.com>
2024-10-06 12:22:12 -0400

    added output message for website

--- .github/actions/deploy/main.js
+++ .github/actions/deploy/main.js
@@ -9,10 +9,14 @@ function run() {
     const distFolder = core.getInput('dist-folder', { require: true });
 
     const s3url = `s3://${bucket}`;
+    //exec command looks for AWS_ACCESS_KEY_ID and AWS_SECERT_ACCESS_KEY variables
     //exec.exec(`aws s3 sync ${distFolder} ${s3url} --region ${bucket}`);
 
-    core.notice(`aws s3 sync ${distFolder} ${s3url} --region ${bucket}`);
+    core.notice(`aws s3 sync ${distFolder} ${s3url} --region ${bucketRegion}`);
     core.notice('Hello from custom javascript action');
+
+    const url = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOutput('website-url', url);
 }
 
 run();
