da50da0700414b32d9f744c9fd7c32219c920e1c
James <jlogan117@gmail.com>
2024-10-06 11:36:20 -0400

    updated custom action

--- .github/actions/deploy/main.js
+++ .github/actions/deploy/main.js
@@ -3,7 +3,16 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello from custom javascript action')
+
+    const bucket = core.getInput('bucket', { require: true });
+    const bucketRegion = core.getInput('bucket-region', { require: true });
+    const distFolder = core.getInput('dist-folder', { require: true });
+
+    const s3url = `s3://${bucket}`;
+    //exec.exec(`aws s3 sync ${distFolder} ${s3url} --region ${bucket}`);
+
+    core.notice(`aws s3 sync ${distFolder} ${s3url} --region ${bucket}`);
+    core.notice('Hello from custom javascript action');
 }
 
 run();
